66e9282b09a5be466573c32a4531bb31e1c7016a
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-26 18:50:52 +0200

    add basic four controllers.

--- BuberBreakfast/Controllers/BreakfastsController.cs
+++ BuberBreakfast/Controllers/BreakfastsController.cs
@@ -8,9 +8,29 @@ namespace BuberBreakfast.Controllers
     public class BreakfastsController : ControllerBase
     {
        [HttpPost("/breakfasts")]
-        public IActionResult Create(CreateBreakfastRequest request)
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
         {
             return Ok();
         }
+
+       [HttpGet("/breakfasts/{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+       
+       [HttpPut("/breakfasts/{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+       
+       [HttpDelete("/breakfasts/{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
     }
+
+
 }
